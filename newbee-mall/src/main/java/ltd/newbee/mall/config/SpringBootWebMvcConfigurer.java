78ced9d8ebbf10ac47dff246e123f74a4d95284c
付宇航 <15774430128@163.com>
2024-03-22 15:28:03 +0800

    files upload

---
+++ newbee-mall/src/main/java/ltd/newbee/mall/config/SpringBootWebMvcConfigurer.java
@@ -0,0 +1,17 @@
+package ltd.newbee.mall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class SpringBootWebMvcConfigurer {
+
+    @Configuration
+    public class NeeBeeMallWebMvcConfigurer implements WebMvcConfigurer {
+
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/upload/**").addResourceLocations("file:D:\\upload\\");
+        }
+    }
+
+}
